222eb4e0fbfb73b0bebb251033cc8172cf751e48
Broderick Johnson <brodjohn22@gmail.com>
2026-09-19 19:21:42 -0300

    Implementing a few different methods

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -35,16 +35,14 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        return null;
-        //throw new RuntimeException("Not implemented");
+        return pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        //throw new RuntimeException("Not implemented");
-        return null;
+        return type;
     }
 
     /**
