9b56f95ba9e4df42d9d0a1ac5171e9e98bc576bf
Broderick Johnson <brodjohn22@gmail.com>
2026-09-19 04:48:54 -0300

    Added Rook moves

---
+++ shared/src/main/java/chess/RookMoves.java
@@ -0,0 +1,31 @@
+package chess;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class RookMoves implements MoveProvider{
+
+    private final int[][] DIRECTIONS = {{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
+
+    public Collection<ChessMove> getValidMoves(ChessBoard board, ChessPosition position){
+        var moves = new ArrayList<ChessMove>();
+
+        for(int[] dir : DIRECTIONS){
+            int multiplier = 1;
+            boolean existed = true;
+            do{
+                var target = new ChessPosition(position.getRow() + (dir[0] * multiplier), position.getColumn() + (dir[1] * multiplier));
+                if(board.positionExists(target) && !board.getPosition(target).hasPiece()){
+                    moves.add(new ChessMove(position, target, null));
+                } else{
+                    existed = false;
+                }
+
+                multiplier++;
+            }while(existed);
+        }
+
+        return moves;
+    }
+
+}
