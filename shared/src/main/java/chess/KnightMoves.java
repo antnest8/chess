22899c19b73993a9a394725eaa904ef73f861cb8
Broderick Johnson <brodjohn22@gmail.com>
2026-09-19 21:45:42 -0300

    Saving changes...

--- shared/src/main/java/chess/KnightMoves.java
+++ shared/src/main/java/chess/KnightMoves.java
@@ -5,24 +5,22 @@ import java.util.Collection;
 
 public class KnightMoves implements MoveProvider{
 
-    private final int[][] DIRECTIONS = {{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
+    private final int[][] DIRECTIONS = {{2, -1}, {2, 1}, {1, 2}, {-1, 2}, {-1, -2}, {1, -2}, {-2, -1}, {-2, 1}};
 
     public Collection<ChessMove> getValidMoves(ChessBoard board, ChessPosition position, ChessGame.TeamColor color){
         var moves = new ArrayList<ChessMove>();
 
         for(int[] dir : DIRECTIONS){
-            int multiplier = 1;
-            boolean existed = true;
-            do{
-                var target = new ChessPosition(position.getRow() + (dir[0] * multiplier), position.getColumn() + (dir[1] * multiplier));
-                if(board.positionExists(target) && !board.getPosition(target).hasPiece()){
-                    moves.add(new ChessMove(position, target, null));
+            var target = new ChessPosition(position.getRow() + dir[0], position.getColumn() + dir[1]);
+            if(board.positionExists(target)){
+                if(board.getPosition(target).hasPiece()){
+                    if(board.getPiece(target).getTeamColor() != color){
+                        moves.add(new ChessMove(position, target, null));
+                    }
                 } else{
-                    existed = false;
+                    moves.add(new ChessMove(position, target, null));
                 }
-
-                multiplier++;
-            }while(existed);
+            }
         }
 
         return moves;
--- shared/src/main/java/chess/PawnMoves.java
+++ shared/src/main/java/chess/PawnMoves.java
@@ -3,27 +3,62 @@ package chess;
 import java.util.ArrayList;
 import java.util.Collection;
 
+/*
+* Everyone's least favorite
+*/
 public class PawnMoves implements MoveProvider{
 
-    private final int[][] DIRECTIONS = {{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
-
-    public Collection<ChessMove> getValidMoves(ChessBoard board, ChessPosition position, ChessGame.TeamColor color){
+    public Collection<ChessMove> getValidMoves(ChessBoard board, ChessPosition position, ChessGame.TeamColor color) {
         var moves = new ArrayList<ChessMove>();
 
-        for(int[] dir : DIRECTIONS){
-            int multiplier = 1;
-            boolean existed = true;
-            do{
-                var target = new ChessPosition(position.getRow() + (dir[0] * multiplier), position.getColumn() + (dir[1] * multiplier));
-                if(board.positionExists(target) && !board.getPosition(target).hasPiece()){
-                    moves.add(new ChessMove(position, target, null));
-                } else{
-                    existed = false;
-                }
-
-                multiplier++;
-            }while(existed);
+        int colorMult;
+        int promotionRow;
+        int startingRow;
+        switch(color){
+            case ChessGame.TeamColor.BLACK -> {
+                colorMult = -1;
+                promotionRow = 1;
+                startingRow = 7;
+            }
+            case ChessGame.TeamColor.WHITE -> {
+                colorMult = 1;
+                promotionRow = 8;
+                startingRow = 2;
+            }
+            default -> {
+                colorMult = 2;
+                promotionRow = 2;
+                startingRow = 3;
+            }
         }
+
+        /*
+        ---------- Basic Forward -------------------------
+         */
+        var target = new ChessPosition(position.getRow() + (colorMult), position.getColumn());
+        if(promotionRow == target.getRow()){
+            moves.add(new ChessMove(position, target, ChessPiece.PieceType.BISHOP));
+            moves.add(new ChessMove(position, target, ChessPiece.PieceType.ROOK));
+            moves.add(new ChessMove(position, target, ChessPiece.PieceType.KNIGHT));
+            moves.add(new ChessMove(position, target, ChessPiece.PieceType.KING));
+            moves.add(new ChessMove(position, target, ChessPiece.PieceType.QUEEN));
+        }
+        else if(!board.getPosition(target).hasPiece()){
+            moves.add(new ChessMove(position, target, null));
+        }
+
+        /*
+        -------- Double jump---------------
+         */
+        target = new ChessPosition(position.getRow() + (colorMult * 2), position.getColumn());
+        var path = new ChessPosition(position.getRow() + (colorMult), position.getColumn());
+        if(position.getRow() == startingRow && !board.getPosition(target).hasPiece() && !board.getPosition(path).hasPiece()){
+            moves.add(new ChessMove(position, target, null));
+        }
+
+        /*
+        ------- Attack --------------------
+         */
 
         return moves;
     }
